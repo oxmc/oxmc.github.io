295c447bf9e9e596391ca8d8f05140b41dfbf607
oxmc <67136658+oxmc@users.noreply.github.com>
2023-09-26 23:31:56 -0700

    Fix some bugs

--- school/sticky_notes.js
+++ school/sticky_notes.js
@@ -16,7 +16,7 @@ function createProjectCard(data) {
             div.classList.add("large-item");
         }
         div2.innerHTML = `<div id="${project.id}-modal" class="modal">
-        <span class="dl"><button class="btn" id="${project.id}-modal-dl"><i class="fa fa-download"></i> Download</button></span>
+        <!--<span class="dl"><button class="btn" id="${project.id}-modal-dl"><i class="fa fa-download"></i> Download</button></span>-->
         <span class="close" id="${project.id}-modal-close">&times;</span>
         <img class="modal-content" id="${project.id}-modal-img">
       </div>`;
