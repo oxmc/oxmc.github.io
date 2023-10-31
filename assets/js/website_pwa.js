f0962ea0c4f1167929b67380315522aa1608e725
oxmc <oxmc7769.mail@gmail.com>
2023-10-31 02:09:19 -0700

    fix pwa

--- assets/js/website_pwa.js
+++ assets/js/website_pwa.js
@@ -11,28 +11,33 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
         });
 }
 
+let firstTime;
+firstTime = true;
 window.addEventListener('beforeinstallprompt', (event) => {
     // Prevent the default installation prompt
     event.preventDefault();
 
-    iziToast.question({
-        timeout: 10000,
-        close: false,
-        overlay: true,
-        displayMode: 'once',
-        layout: 2,
-        id: 'pwa-question',
-        title: 'Install site as PWA',
-        message: 'Would you like to install my site as a PWA?',
-        position: 'center',
-        buttons: [
-            ['<button style="float: right !important;"><b>YES</b></button>', function (instance, toast) {
-                instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-                event.prompt();
-            }, true],
-            ['<button style="float: right !important;">NO</button>', function (instance, toast) {
-                instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-            }],
-        ]
-    });
+    if (firstTime === true) {
+        iziToast.question({
+            timeout: 10000,
+            close: false,
+            overlay: true,
+            displayMode: 'once',
+            layout: 2,
+            id: 'pwa-question',
+            title: 'Install site as PWA',
+            message: 'Would you like to install my site as a PWA?',
+            position: 'center',
+            buttons: [
+                ['<button style="float: right !important;"><b>YES</b></button>', function (instance, toast) {
+                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                    firstTime = false;
+                    event.prompt();
+                }, true],
+                ['<button style="float: right !important;">NO</button>', function (instance, toast) {
+                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                }],
+            ]
+        });
+    }
 });
