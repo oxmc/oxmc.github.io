656e9c579afaa74450f9d67c3e0177aa582c977e
oxmc <oxmc7769.mail@gmail.com>
2023-10-30 23:55:14 -0700

    Update website_pwa.js

--- assets/js/website_pwa.js
+++ assets/js/website_pwa.js
@@ -13,24 +13,24 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 
 window.addEventListener('beforeinstallprompt', (event) => {
     // Prevent the default installation prompt
-    //event.preventDefault();
+    event.preventDefault();
 
     // Store the event for later use
     window.installPromptEvent = event;
 
     // You can choose when and how to display the install prompt, e.g., by adding a button or automatically
     // For example, you can add a button that, when clicked, triggers the install prompt
-    // const installButton = document.getElementById('install-button');
-    // installButton.style.display = 'block';
+    const installButton = document.getElementById('install-button');
+    installButton.style.display = 'block';
 
-    // installButton.addEventListener('click', () => {
-    //   event.prompt();
-    //   event.userChoice
-    //     .then((choiceResult) => {
-    //       if (choiceResult.outcome === 'accepted') {
-    //         console.log('User accepted the installation prompt');
-    //       }
-    //       window.installPromptEvent = null; // Reset the event
-    //     });
-    // });
+    installButton.addEventListener('click', () => {
+        event.prompt();
+        event.userChoice
+            .then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the installation prompt');
+                }
+                window.installPromptEvent = null; // Reset the event
+            });
+    });
 });
